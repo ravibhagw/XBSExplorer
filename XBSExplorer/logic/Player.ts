3cfbe229cd57cae85d3ea9d8d624c0e847052661
Ravindra Bhagwandin <rbhagwandin@gmail.com>
2021-02-03 14:31:51 -0500

    Map raw data to "Player" class.  Refine paging naming convention.

---
+++ XBSExplorer/logic/Player.ts
@@ -0,0 +1,9 @@
+class Player {
+    gamertag: string;
+    forumId: string;
+
+    constructor(gamertag: string, forumId: string) {
+        this.gamertag = gamertag;
+        this.forumId = forumId;
+    }
+}
